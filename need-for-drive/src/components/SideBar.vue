1eff3875054b5f7e7e2901c3d13465a45f3a5d4e
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-10-30 13:32:31 +0300

    added sidebar component
    added SideBar component, Main view, removed unuses components

---
+++ need-for-drive/src/components/SideBar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="side-bar">
+    <div class="side-bar__menu-wrapper menu-wrapper" v-on:click="isActive = !isActive">
+      <div class="menu-wrapper__hamburger-menu hamburger-menu" v-bind:class="{ 'hamburger-menu--active': isActive }"></div>
+    </div>
+    <button class="side-bar__lang-button">
+      Eng
+    </button>
+    <div class="nav-bar" v-bind:class="{ 'nav-bar--active': isActive }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  data() {
+    return {
+      isActive: false
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "public/styles/menu";
+.side-bar {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 65px;
+  height: 100vh;
+  background-color: #151b1f;
+  padding: 30px 0 30px 0;
+}
+
+.side-bar__lang-button {
+  background-color: #151b1f;
+  color: #0ec261;
+  font-size: 13px;
+  width: 35px;
+  padding: 5px;
+  font-weight: bold;
+  cursor: pointer;
+  border: none;
+
+  &:hover {
+    color: #fff;
+    transition: color 0.3s;
+  }
+}
+
+.nav-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #151b1f;
+  height: 100vh;
+  width: 100vw;
+  display: none;
+}
+
+.nav-bar--active {
+  display: block;
+  opacity: 0.98;
+}
+
+/*@keyframes menu {*/
+/*  0% { opacity: 0; }*/
+/*  100% { opacity: 0.9; }*/
+/*}*/
+</style>
---
+++ need-for-drive/src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-content">
+    <NavBar />
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/SideBar";
+export default {
+  name: "Main",
+  components: { NavBar }
+};
+</script>
+
+<style scoped>
+.main-content {
+  display: flex;
+  flex-direction: row;
+}
+</style>
