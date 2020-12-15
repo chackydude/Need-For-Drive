2376b3cf75016b9dc4975fab44c10a8af06efdf5
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-12-15 14:59:05 +0300

    fixes

--- need-for-drive/src/router/index.js
+++ need-for-drive/src/router/index.js
@@ -29,13 +29,14 @@ const routes = [
     path: "/admin/*",
     component: () => import("../views/Admin"),
     // route check function
-    beforeEnter(to, from, next) {
-      if (localStorage.getItem("authId")) {
-        next();
-      } else {
-        next({ name: "auth" });
-      }
-    }
+
+    // beforeEnter(to, from, next) {
+    //   if (localStorage.getItem("authId")) {
+    //     next();
+    //   } else {
+    //     next({ name: "auth" });
+    //   }
+    // }
   }
 ];
 
