b9786037e2abe596d30c7da431c0e5bfae36de30
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-01-27 15:20:25 +0300

    fixes

--- need-for-drive/src/utils/api/instance.js
+++ need-for-drive/src/utils/api/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  // baseURL: process.env.VUE_APP_CORS_URL + process.env.VUE_APP_BASE_URL,
-  baseURL: process.env.VUE_APP_BASE_URL,
+  baseURL: process.env.VUE_APP_CORS_URL + process.env.VUE_APP_BASE_URL,
+  // baseURL: process.env.VUE_APP_BASE_URL,
   headers: {
     "X-Api-Factory-Application-Id": process.env.VUE_APP_ID,
     Authorization: "Bearer " + process.env.VUE_APP_SECRET_KEY,
